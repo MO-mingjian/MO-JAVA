3a98ab5f6e06a8745c828b8cde6431052801bfd1
MO-mingjian <74659994+MO-mingjian@users.noreply.github.com>
2020-11-23 11:51:40 +0800

    Update 112301.App.java
    使用java设计一个随机坐标生成程序

--- 112301.App.java
+++ 112301.App.java
@@ -7,10 +7,13 @@ public class APP {
 	public static void main(String[] args) {
 		while (true){
         Scanner scanner=new Scanner(System.in);
-        int a=scanner.nextInt();
-        int x=(int)Math.floor(Math.random()*(100));
-        int y=(int)Math.floor(Math.random()*(100));
+        int i=scanner.nextInt();
+        if(i==1) {
+        	 int x=(int)Math.floor(Math.random()*(101));
+             int y=(int)Math.floor(Math.random()*(101));
         System.out.println("R("+x+","+y+")");
 	}}
 
 }
+}
+
